08111f8fdf9c7f94624f5ecb3c682436b0d107e9
patrick tully <patrick@avatarnewyork.com>
2015-04-21 12:17:00 -0400

    fixed!

--- public/link1.php
+++ public/link1.php
@@ -1,7 +1,7 @@
 <html>
 <head><title>Link1 Page</title></head>
 <body>
-Script Name: <div id="script"><? print $_SERVER["SCRIPT_NAME"]); ?></div>
+Script Name: <div id="script"><? print($_SERVER["SCRIPT_NAME"]); ?></div>
 <ul>
 <li><a href="link1.php">link1</a>
 <li><a href="link2.php">link2</a>
