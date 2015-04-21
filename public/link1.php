f9eac794756cdcdcbcb6458e5545c66c061d8ee7
patrick tully <patrick@avatarnewyork.com>
2015-04-21 11:19:04 -0400

    Added some dummy files

---
+++ public/link1.php
@@ -0,0 +1,13 @@
+<html>
+<head><title>Link1 Page</title></head>
+<body>
+Script Name: <? print($_SERVER["SCRIPT_NAME"]); ?>
+<ul>
+<li><a href="link1.php">link1</a>
+<li><a href="link2.php">link2</a>
+</ul>
+<br/>
+Go back <a href="/">home</a>
+</body>
+</html>
+
